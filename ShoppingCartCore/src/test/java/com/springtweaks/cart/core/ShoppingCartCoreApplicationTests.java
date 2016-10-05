026290aa717078e3ce56f809f4e37972d6e5254c
rkaranam <karanam.rajasekhar44@gmail.com>
2016-10-05 20:29:13 +0530

    Base Environment Setup

--- ShoppingCartCore/src/test/java/com/springtweaks/cart/core/ShoppingCartCoreApplicationTests.java
+++ ShoppingCartCore/src/test/java/com/springtweaks/cart/core/ShoppingCartCoreApplicationTests.java
@@ -1,7 +1,14 @@
 package com.springtweaks.cart.core;
 
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +16,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ShoppingCartCoreApplicationTests {
 
+	@Autowired DataSource dataSource;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testDummy() throws SQLException {
+		String schema = dataSource.getConnection().getCatalog();
+		assertEquals("shopping-cart", schema);
 	}
 
 }
